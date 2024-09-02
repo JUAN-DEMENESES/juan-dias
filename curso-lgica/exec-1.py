3dc8cdf76d37de7131597abde630310988d1908f
juan de meneses dias <diasmenesesdias@gmail.com>
2024-09-02 19:51:09 -0300

    exec-1.py

---
+++ curso-lgica/exec-1.py
@@ -0,0 +1,41 @@
+Questão 1
+print("o primeiro programa a gente nunca esquece")
+
+
+Questão 3
+print("you how do you own the world?how do you own disorder")
+Questão 4
+print("xxxx")
+print("x  x")
+print("x  x")
+print("x  x")
+print("xxxx")
+Questão 5
+print("Aluno Nota")
+print("===== =====")
+print("aline 9,0") 
+print("Mário dez") 
+print("Sérgio 4,5")
+print("Shirley 7,0")
+Questão 6
+print("Cadastro de Clientes")
+print("0 - Fim") 
+print("1 - Inclui")
+print("2 - Altera")
+print("3 - Exclui")
+print("4 - Consulta")
+Questão 7
+print ("        X")
+print ("       XXX ")
+print ("      XXXXX") 
+print ("     XXXXXXX")
+print ("    XXXXXXXXX")
+print ("   XXXXXXXXXXX")
+print ("  XXXXXXXXXXXXX")
+print (" XXXXXXXXXXXXXXX")
+print ("       XX")
+print ("       XX")
+print ("      XXXX")
+QUESTÃO 8
+print("Qual é o seu nome?")
+input()
